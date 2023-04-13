c82c6de625f3142ac9ce0ad57b68667c2d133c65
ChurchillV <vinchurch70@gmail.com>
2023-04-13 20:01:37 +0000

    A better version

--- main.c
+++ main.c
@@ -1,265 +1,270 @@
 #include <stdio.h>
 #include <string.h>
-#include <stdlib.h>
 
-struct SHOP
-{
+struct SHOP {
     char name[50];
     char contact[12];
-}available_shops[1000];
+};
 
-struct CLOTHING
-{
+struct CLOTHING {
     char name[20];
-    char code[5];
+    char code[6];
     char year[5];
-    struct SHOP shop_sold;
-
+    struct SHOP shop;
     struct PRICE
     {
-        char currency[3];
-        char amount[10];
+        char currency[4];
+        float amount;
     }price;
-
     union SIZE
     {
         int digit;
         char letter;
-        char word[20];
+        char word[10];
     }size;
-    
     int sizetype;
-}available_clothing[1000];
+};
 
-int shopCount = 2, clothesCount = 4;
+struct SHOP available_shops[1000];
+struct CLOTHING available_clothing[1000];
+int shopCount = 0; 
+int clothesCount = 0;
 
-//Function declarations
 void showShops();
-void showDetails(int shop);
-void showClothes(int shop);
+void showDetails(int option);
+void showClothes(int option);
 void addClothing();
 
-
 int main() {
-
     struct SHOP shop_1, shop_2;
     strcpy(shop_1.name, "Vince's Couture");
     strcpy(shop_1.contact, "0541344662");
-    strcpy(shop_2.name, "Keyshia's Couture");
-    strcpy(shop_2.contact, "0595623451");
+    strcpy(shop_2.name, "Sharons's Couture");
+    strcpy(shop_2.contact, "0602561234");
 
     available_shops[0] = shop_1;
     available_shops[1] = shop_2;
+    shopCount = 2;
 
-    struct CLOTHING item_1, item_2, item_3, item_4;
-    //Item 1
+    struct CLOTHING item_1,item_2,item_3,item_4;
     strcpy(item_1.name, "Tank Top");
-    strcpy(item_1.code, "hx123");
-    strcpy(item_1.year, "2016");
-    item_1.shop_sold = shop_1;
-    strcpy(item_1.price.currency, "GHC");
-    strcpy(item_1.price.amount, "300");
-    item_1.sizetype = 2;
+    strcpy(item_1.code, "xy123");
+    strcpy(item_1.year, "2019");
+    item_1.shop = shop_1;
+    strcpy(item_1.price.currency, "USD");
+    item_1.price.amount = 150.00;
     strcpy(item_1.size.word, "Large");
-    //Item 2
-    strcpy(item_2.name, "Lingerie");
-    strcpy(item_2.code, "rt567");
-    strcpy(item_2.year, "2019");
-    item_2.shop_sold = shop_1;
-    strcpy(item_2.price.currency, "USD");
-    strcpy(item_2.price.amount, "45");
-    item_2.sizetype = 0;
-    item_2.size.digit = 40;
-    //Item 3
+    item_1.sizetype = 2;
+    
+    strcpy(item_2.name, "Booty Shorts");
+    strcpy(item_2.code, "ab789");
+    strcpy(item_2.year, "2023");
+    item_2.shop = shop_1;
+    strcpy(item_2.price.currency, "GHC");
+    item_2.price.amount = 50.00;
+    item_2.size.letter = 'M';
+    item_2.sizetype = 1;
+
     strcpy(item_3.name, "Hoodie");
-    strcpy(item_3.code, "kt153");
-    strcpy(item_3.year, "2010");
-    item_3.shop_sold = shop_2;
-    strcpy(item_3.price.currency, "GHC");
-    strcpy(item_3.price.amount, "30");
+    strcpy(item_3.code, "gf567");
+    strcpy(item_3.year, "2021");
+    item_3.shop = shop_2;
+    strcpy(item_3.price.currency, "EUR");
+    item_3.price.amount = 10.00;
+    item_3.size.letter =  'L';
     item_3.sizetype = 1;
-    strcpy(item_3.size.word, "L");
-    //Item 4
-    strcpy(item_4.name, "Joggers");
-    strcpy(item_4.code, "jcs13");
-    strcpy(item_4.year, "2016");
-    item_4.shop_sold = shop_2;
-    strcpy(item_4.price.currency, "GHC");
-    strcpy(item_4.price.amount, "300");
-    item_4.sizetype = 2;
-    strcpy(item_4.size.word, "Small");
+
+    strcpy(item_4.name, "Sweat Pants");
+    strcpy(item_4.code, "we345");
+    strcpy(item_4.year, "2020");
+    item_4.shop = shop_2;
+    strcpy(item_4.price.currency, "USD");
+    item_4.price.amount = 50.00;
+    item_4.size.digit = 50;
+    item_4.sizetype = 0;
 
     available_clothing[0] = item_1;
     available_clothing[1] = item_2;
     available_clothing[2] = item_3;
     available_clothing[3] = item_4;
 
+    clothesCount = 4;
+
+
     int looping = 0;
     do {
-        printf("\n\nCLOTHING MALL\n1. Visit Shop\t2.Add Clothing\t3.Exit\n");
-        int option = 0;
-        scanf(" %d", &option);
-        switch (option) 
+        printf("\nCLOTHING MALL\n\n1 - Visit Shop\t2 - Add Clothing\t3 - Exit\n");
+        int option;
+        fflush(stdin);
+        scanf(" %d", &looping);
+        switch (looping)
         {
         case 1:
             showShops();
             scanf(" %d", &option);
             showDetails(option - 1);
             showClothes(option - 1);
-            looping = 1;
             break;
-
-        case 2:
+        case 2: 
             addClothing();
-            looping = 1;
             break;
-
-        case 3: looping = 0; printf("Thanks for shopping ;)"); break;
-        
+        case 3:
+            looping = 0;
+            printf("\nThanks for shopping :)");
+            break;
         default:
-        printf("Invalid choice\n");break;
+            printf("Invalid choice\n");
+            break;
         }
-    }while (looping > 0);
+    } while (looping > 0);
+
 
     return 0;
 }
 
-//Function Definitions
-
-//Display available shops
 void showShops() {
-    printf("\nAVAILABLE SHOPS\n");
-    printf("------------------------\n");
-    for (int i = 0; i < shopCount; i++)
-    {
-        printf("%d\t%s\n", i+1, available_shops[i].name);
+    printf("\n\nAVAILABLE SHOPS\n");
+    for (int i = 0; i < shopCount; i++) {
+        printf("\n%d - %s", i + 1, available_shops[i].name);
     }
-    printf("\n\nWhich one would you like to visit?\n\nOption: ");
+    printf("\n\nSelect shop to visit: ");
 }
 
-//Show the details of a particular shop
-void showDetails(int shop) {
-    printf("\n%s\t%s\n", available_shops[shop].name, available_shops[shop].contact);
-
+void showDetails(int option) {
+    printf("\n\nSHOP %d\n\n", option + 1);
+    printf("NAME : %s\nCONTACT : %s", available_shops[option].name, available_shops[option].contact);
 }
 
-//Display the clothing available in a particular shop
-void showClothes(int shop) {
-    printf("\n\nAVAILABLE CLOTHING\n");
-    printf("------------------------\n");
-    fflush(stdin);
+void showClothes(int option) {
+    printf("\n\nAVAILABLE CLOTHING\n\n");
     for (int i = 0; i < clothesCount; i++)
     {
-        if (strcmp(available_clothing[i].shop_sold.name,available_shops[shop].name) == 0)
-        {
-            printf("Item Name: %s\n", available_clothing[i].name);
-            printf("Item Code: %s\n", available_clothing[i].code);
-            printf("Production Year: %s\n", available_clothing[i].year);
-            printf("Price: %s\n", available_clothing[i].price.currency);
+        if (strcmp(available_clothing[i].shop.name,available_shops[option].name) == 0) {
+            printf("ITEM NAME: %s\n", available_clothing[i].name);
+            printf("ITEM CODE: %s\n", available_clothing[i].code);
+            printf("YEAR OF PRODUCTION: %s\n", available_clothing[i].year);
+            printf("ITEM PRICE: %s %.2f\n", available_clothing[i].price.currency,available_clothing[i].price.amount);
+            printf("ITEM SIZE : ");
             switch (available_clothing[i].sizetype)
             {
             case 0:
-                printf("Size: %d\n\n\n",available_clothing[i].size.digit);
+                printf("%d\n\n", available_clothing[i].size.digit);
                 break;
-
             case 1:
-                printf("Size: %c\n\n\n",available_clothing[i].size.letter);
+                printf("%c\n\n", available_clothing[i].size.letter);
                 break;
-
             case 2:
-                printf("Size: %s\n\n\n",available_clothing[i].size.word);
+                printf("%s\n\n", available_clothing[i].size.word);
                 break;
             
             default:
+                printf("UNAVAILABLE\n\n");
                 break;
             }
         }
-        
     }
     
 }
-//Add a new set of clothing to the app database
+
+//Add a new clothing item
 void addClothing() {
-    printf("How many new items would you like to add?\n");
-    int num, size_1, option, sizetype;
-    char name[50], code[5], year[4], currency[3], amount[10], size_3[10], size_2;
+    char name[50], code[6], year[5], currency[4], letter, word[20];
+    int digit, amount, sizetype, option, item_no, looping = 0;
+    do {printf("\nNumber of items to add: ");
     fflush(stdin);
-    printf("Enter amount: ");
-    scanf(" %d", &num);
+    scanf(" %d", &item_no);
 
-    for (int i = clothesCount; i < clothesCount + num; i++)
+    for (int i = 0; i < item_no; i++) {
+    printf("\n\nUPDATE CLOTHING INVENTORY\n\n");
+    printf("Enter item name: ");
+    fflush(stdin);
+    scanf(" %s", &name);
+    strcpy(available_clothing[clothesCount + i].name, name);
+    printf("\n\nEnter item code: ");
+    fflush(stdin);
+    scanf(" %s", &code);
+    strcpy(available_clothing[clothesCount + i].code, code);
+    printf("\n\nYear of Production: ");
+    fflush(stdin);
+    scanf(" %s", &year);
+    strcpy(available_clothing[clothesCount + i].year, year);
+    printf("\n\nEnter price (amount): ");
+    fflush(stdin);
+    scanf(" %f", &amount);
+    available_clothing[clothesCount + i].price.amount = amount;
+    printf("\n\nEnter Currency (Eg. USD, GHC): ");
+    fflush(stdin);
+    scanf(" %s", &currency);
+    strcpy(available_clothing[clothesCount + i].price.currency, currency);
+
+    do {printf("\n\nSelect size type\n1 - Digit\n2 - Letter\n3 - Words\n\nYour choice: ");
+    fflush(stdin);
+    scanf(" %d", &sizetype);
+    available_clothing[clothesCount + i].sizetype = sizetype - 1;
+    switch (sizetype)
     {
-        printf("\nItem Name: ");
-        fflush(stdin);
-        gets(name);
-        strcpy(available_clothing[i].name, name);
-        fflush(stdin);
-        printf("\nItem ID: ");
-        gets(code);
-        strcpy(available_clothing[i].code, code);
-        fflush(stdin);
-        printf("\nItem Production Year: ");
-        gets(year);
-        strcpy(available_clothing[i].year, year);
-        fflush(stdin);
-        printf("\nPrice (Currency): ");
-        gets(currency);
-        strcpy(available_clothing[i].price.currency, currency);
-        printf("\nPrice (Amount): ");
-        gets(amount);
-        strcpy(available_clothing[i].price.amount, amount);
+    case 1:
+        printf("\n\nEnter item size (Digit): ");
+        scanf(" %d", &digit);
+        available_clothing[clothesCount + i].size.digit = digit;
+        break;
+    case 2:
+        printf("\n\nEnter item size (Letter): ");
+        scanf(" %c", &letter);
+        available_clothing[clothesCount + i].size.letter = letter;
+        break;
+    case 3:
+        printf("\n\nEnter item size (Words): ");
+        scanf(" %s", &word);
+        strcpy(available_clothing[clothesCount + i].size.word, word);
+        break;
+    
+    default:
+        printf("Invalid option");
+        looping = 2;
+        break;
+    }} while (looping == 2);
+    
+    do {printf("\n\nSelect shop\n\n1 - Vince's Couture\n2 - Sharon's Couture\n\nYour choice: ");
+    fflush(stdin);
+    scanf(" %d", &option);
+    switch (option)
+    {
+    case 1:
+        available_clothing[clothesCount + i].shop = available_shops[option - 1];
+        break;
+    case 2:
+        available_clothing[clothesCount + i].shop = available_shops[option - 1];
+        break;
+    
+    default:
+        printf("\n\nInvalid option");
+        looping = 2;
+        break;
+    }} while (looping == 2);
 
-        do {printf("\nSizing System:\n1. Digits\n2. Letter\n3. Word\n\nEnter option: ");
-            fflush(stdin);
-            scanf(" %d", &sizetype);
-            available_clothing[i].sizetype = sizetype - 1;
-            switch (sizetype)
-            {
-            case 1:
-                printf("\nEnter size: ");
-                fflush(stdin);
-                scanf(" %d", &size_1);
-                available_clothing[i].size.digit = size_1;
-                break;
-
-            case 2:
-                printf("\nEnter size: ");
-                fflush(stdin);
-                scanf(" %c", &size_2);
-                available_clothing[i].size.letter = size_2;
-                break;
-
-            case 3:
-                printf("\nEnter size: ");
-                fflush(stdin);
-                scanf(" %s", &size_3);
-                strcpy(available_clothing[i].size.word, size_3);
-                break;
-            
-            default:
-                printf("\nPlease enter a valid option");
-                break;
-        }} 
-        while (sizetype > 3 && sizetype < 1);
-
-        printf("\nSelect Shop where item is sold: \n");
-        for (int i = 0; i < shopCount; i++)
-        {
-            printf("\n%d %s\n", i+1, available_shops[i].name);
-        }
-
-        do {printf("Select shop: ");
-            fflush(stdin);
-            scanf(" %d", &option);
-            if (option < shopCount + 1) {
-                    strcpy(available_clothing[i].shop_sold.name, available_shops[option-1].name);
-                }
-            else {
-                printf("\nPlease enter a valid option");
-            }}
-        while (option > shopCount && option < 0);
-        printf("\nItem successfully added");
+    clothesCount += 1;
+    printf("\n\nInventory updated. Available clothes: %d", clothesCount);
     }
-    clothesCount += num;
-    printf("\nClothing Items successfully added");
+    printf("\n\nItems added successfully\n");
+
+    do {printf("\n\nAdd more items?\n\n1 - YES\n2 - NO\n\nYour choice: ");
+    fflush(stdin);
+    scanf(" %d", &option);
+    switch (option)
+    {
+    case 1:
+        printf("\nAlright! Let's do this again...");
+        looping = 1;
+        break;
+    case 2:
+        printf("\nExiting...");
+        looping = 0;
+        break;
+    default:
+        printf("\nInvalid option");
+        looping = 2;
+        break;
+    }} while (looping == 2);
+    } while(looping == 1);
 }
